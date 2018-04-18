8c83e7c0ceee14330baf0ec804a8c4e00d3174d5
Henry Hsu <h_hsu817@yahoo.com>
2018-04-18 20:56:37 +0000

    Task 5. Implement endpoint to return number of objects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,22 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+
+classes = {"User": "users", "Place": "places", "State": "states",
+           "City": "cities", "Amenity": "amenities", "Review": "reviews"}
 
 
 @app_views.route("/status")
 def status():
     """ Returns a JSON of status """
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats")
+def stats():
+    """ Retrieves the number of each objects by type """
+    total_by_type = {}
+    for entry in classes.keys():
+        total_by_type[classes[entry]] = storage.count(entry)
+    return jsonify(total_by_type)
