1e79c09ce37f48cd1279baa7fa6fe0ad6aa05be2
imperfectskillz <j.choi.89@gmail.com>
2018-04-25 08:11:14 +0000

    changed argument

--- api/v1/app.py
+++ api/v1/app.py
@@ -13,7 +13,7 @@ cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown_appcontext(code):
+def teardown_appcontext(error):
     """ Closes storage """
     storage.close()
 
